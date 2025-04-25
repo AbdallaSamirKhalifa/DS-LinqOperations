ed20cfb94783d44609451294b83cf93c6c68754d
AbdallaSamirKhalifa <abdallasamirkhalifa@gmail.com>
2025-04-25 18:49:33 +0300

    IEnumerable-ICollection

---
+++ CollectionInterfaces/ICollectionInterface/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
+
+internal class Program
+{
+
+    public class SimpleCollection<T> : ICollection<T>
+    {
+        private List<T> items = new List<T>();
+
+        public int Count => items.Count;
+        public bool IsReadOnly => false;
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Clear() 
+        {
+            items.Clear(); 
+        }
+
+        public bool Contains(T item)
+        {
+            return items.Contains(item);
+        }
+        public bool Remove(T item)
+        {
+            return items.Remove(item);
+        }
+
+        public void CopyTo(T[] arr, int index)
+        {
+            items.CopyTo(arr, index);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        
+    }
+
+    static void Main(string[] args)
+    {
+        SimpleCollection<string> shoppingCart = new SimpleCollection<string>();
+        shoppingCart.Add("Apple");
+        shoppingCart.Add("Banana");
+        shoppingCart.Add("Carrot");
+
+        Console.WriteLine($"Items in shopping cart: {shoppingCart.Count}");
+
+        if (shoppingCart.Contains("Banana"))
+        {
+            shoppingCart.Remove("Banana");
+            Console.WriteLine("Banan removed from the cart");
+
+        }
+
+        Console.WriteLine("\nFinal items in the cart:");
+        foreach (var item in shoppingCart)
+        {
+            Console.WriteLine(item);
+
+        }
+
+
+
+
+
+
+        Console.ReadKey();
+    }
+}
+
---
+++ CollectionInterfaces/IEnumerableInterface/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+internal class Program
+{
+    public class CutomCollection <T>: IEnumerable<T>
+    {
+        private List<T> items = new List<T>();
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                /*
+                 * Usage of yield return: it is used within an iterator block to provide a value 
+                 * to the enumerator object and then pauses the execution of the iterator block
+                 * untill the next element is requested.
+                 * Functionality: when a yield return statement is executed,
+                 * it returns the current value to the caller but remembers the curren location
+                 * in code for the next call. the next time the iterator is accessed,
+                 * it resumees execution from the state it was left in,
+                 * immediatly after the last yeild return statement.
+                 */
+
+
+                yield return items[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() 
+        { 
+             return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+    } 
+
+
+     static void Main(string[] args)
+     {
+        var list = new List<int>() { 1, 2, 3 };
+
+      
+
+         var myCollection = new CutomCollection<int>();
+        myCollection.Add(1);
+        myCollection.Add(2);
+        myCollection.Add(3);
+
+        var enumerator = myCollection.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            Console.WriteLine(enumerator.Current);
+        }
+
+        foreach (var item in myCollection)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadKey();
+     }
+    
+}
